ea56ff6d3e3c45d92164bd8b3c861271424b97c0
Sarah Amirie <asamirie@gmail.com>
2017-05-02 18:08:40 -0400

    commit with js file

--- product.sql
+++ product.sql
@@ -1,16 +1,13 @@
 SELECT * FROM bamazon.products;
 
  INSERT INTO bamazon.products(ProductName, DepartmentName, Price, StockQuantity)
- 
+
  values("Monopoly", "Board Games", 30, 12, 16),
-			("iphone 7", "Phone", 15, 299, 100),
-            ("MacBook Pro", "Electronics", 15, 1000, 200),
-            ("Vitamix", "Bender", 20, 450, 31),
-            ("Ray Bans", "Accessories", 11, 150, 11),
-            ("Perfume", "Beauty", 13, 89, 63),
-            ("ipad", "Electronics", 15, 201, 15);
-            
+			  ("iphone 7", "Phone", 15, 299, 100),
+        ("MacBook Pro", "Electronics", 15, 1000, 200),
+        ("Vitamix", "Bender", 20, 450, 31),
+        ("Ray Bans", "Accessories", 11, 150, 11),
+        ("Perfume", "Beauty", 13, 89, 63),
+        ("ipad", "Electronics", 15, 201, 15);
+
 Select * From products;
-            
-            
- 
